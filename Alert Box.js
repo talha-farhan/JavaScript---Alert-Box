596ec0d0c04efcdd0da2202a21cb8549e6067f96
Talha Farhan <128322158+talha-farhan@users.noreply.github.com>
2023-07-24 09:47:06 +0500

    Create Alert Box.js

---
+++ Alert Box.js
@@ -0,0 +1,19 @@
+alert("Welcome to my profile!");
+
+let usmanAnwar = 250000;
+let aliHassan = 45000;
+if(usmanAnwar > aliHassan){
+    alert("Usman is rich");
+}else{
+    alert("Ali is rich");
+}
+//A variable or an arithmetic operator or cancatenation can also be used instead of a message
+
+let a = 10;
+let b = 20;
+alert(a + b);
+//Multiple alerts can be given in a single file
+
+let income = 400000;
+let expenses = 200000;
+alert("savings are " + (income - expenses));
